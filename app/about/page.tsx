105e572a94708eb0734505291019d456c597c7f7
Jamie Ohakwe <jamieohakwe@gmail.com>
2024-06-22 18:07:53 +0100

    slight adjustment

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,8 +7,8 @@ const About = () => {
   return (
     <>
       <NavBar/>
-      {/* <SectionBanner/> */}
-      <SignupCard/>
+      <SectionBanner/>
+      {/* <SignupCard/> */}
     </>
   )
 }
--- components/SectionBanner/SectionBanner.tsx
+++ components/SectionBanner/SectionBanner.tsx
@@ -6,11 +6,11 @@ const SectionBanner = () => {
     <>
         <div className='h-[200px] w-full relative'>
             <div className='z-10 absolute text-white  text-[2rem] pl-[6rem] pt-[4rem] '>
-                <span>About Us</span>
+                <span>About us</span>
             </div>
             <Image src="/img/facility2.jpg" fill className='absolute object-cover ' alt='banner'/>
             <div className='bg-black opacity-90 w-full h-[200px]'></div>
-            <h1>where are you</h1>
+            <h1>Still loading...</h1>
         </div>
     </>
   )
